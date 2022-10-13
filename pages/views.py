714a5335fab1284cc40552e1eec5d6c16bedab8e
lucky9506133607 <63451625+lucky9506133607@users.noreply.github.com>
2022-10-13 21:46:08 +0530

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -8,14 +8,4 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):  # new
     template_name = "about.html"
     
-if request.method == 'POST' and 'run_script' in request.POST:
-    # import function to run 
-    import seleniumcode 
- 
-    # call function 
-    hrms()  
- 
-    # return user to required page 
-    #return HttpResponseRedirect(reverse('registered'))
-    
         
