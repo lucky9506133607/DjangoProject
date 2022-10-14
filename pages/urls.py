50e7f23f30d63065156dd4cea15987376cd50588
lucky9506133607 <63451625+lucky9506133607@users.noreply.github.com>
2022-10-14 20:06:39 +0530

    update url package

--- pages/urls.py
+++ pages/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path as url
 from django.contrib import admin
 from . import views
 urlpatterns = [
@@ -6,3 +6,5 @@ urlpatterns = [
     url(r'^$', views.button),
     url(r'^output', views.output,name="script"),
 ]
+
+
