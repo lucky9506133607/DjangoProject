6d97d5e527fc12cf72bc5fab8f06add25f9dc07c
lucky9506133607 <63451625+lucky9506133607@users.noreply.github.com>
2022-10-14 23:46:25 +0530

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -1,14 +1,5 @@
 from django.views.generic import TemplateView
 import requests
-from django.shortcuts import render
-import selenium
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-import os
-from selenium.webdriver.common.by import By
-import time
-
 
 class HomePageView(TemplateView):
     template_name = "home.html"
