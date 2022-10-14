d5a0674ba641f3b4f1b03175a74648054c01b2ba
lucky9506133607 <63451625+lucky9506133607@users.noreply.github.com>
2022-10-14 22:06:22 +0530

    add print on all function

--- pages/tests.py
+++ pages/tests.py
@@ -23,17 +23,21 @@ class HomepageTests(SimpleTestCase):
 class AboutpageTests(SimpleTestCase):
     def test_url_exists_at_correct_location(self):
         response = self.client.get("/about/")
+        print("hello world from tests.py file first function")
         self.assertEqual(response.status_code, 200)
 
     def test_url_available_by_name(self):
         response = self.client.get(reverse("about"))
+        print("hello world from tests.py file second function")
         self.assertEqual(response.status_code, 200)
 
     def test_template_name_correct(self):  # new
         response = self.client.get(reverse("about"))
+        print("hello world from tests.py file third function")
         self.assertTemplateUsed(response, "about.html")
 
     def test_template_content(self):  # new
         response = self.client.get(reverse("about"))
+        print("hello world from tests.py file fourth function")
         self.assertContains(response, "<h1>About page</h1>")
-        print("hello world from tests.py file")
+        
