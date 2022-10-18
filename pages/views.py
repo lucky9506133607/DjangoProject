00af802abcf53a4113a5583ef6fffa87eada0569
lucky9506133607 <ls2170184@gmail.com>
2022-10-18 14:42:11 +0530

    test2

--- pages/views.py
+++ pages/views.py
@@ -43,13 +43,13 @@ def HomeView(request):
         #-----------------driver.find_element(By.XPATH, '//*[@id="txtpassword"]').send_keys(Valid_pass[0])
         #-----------------print('pass name enter')
         #-----------------driver.find_element(By.XPATH, '//*[@id="btnlogin"]').click()
-        driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[2]').click()
+        driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[2]').click()
         print('click on button')
-        #try:
-            #driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[2]').click()
+        try:
+            driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[2]').click()
          #   driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[2]').click()
-        #except:
-         #   print('exception occured')
+        except:
+            print('exception occured')
         print('successfull login')
         time.sleep(10)
         print(driver.title)
